4223313cd2d0bb16af831c0cbc0fb586529cf799
Sidney Silva <76785419+Sidneyfib@users.noreply.github.com>
2021-01-15 12:36:23 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -14,15 +14,13 @@ app.get("/repositories", (request, response) => {
  
   return response.json(repositories);
  
- 
- 
   
 });
 
 app.post("/repositories", (request, response) => {
-  const {title, url, techs, likes} = request.body;
+  const {title, url, techs} = request.body;
 
-  const project = {id: uuid(), title, url, techs, likes};
+  const project = {id: uuid(), title, url, techs, likes:0};
 
   repositories.push(project);
 
@@ -31,11 +29,12 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
  const{id} = request.params;
- const {title, url, techs, likes} = request.body;
+ const {title, url, techs} = request.body;
  
  const projectIndex = repositories.findIndex(project => project.id === id);
+ 
 
- if(projectIndex <0){
+ if(projectIndex ===-1){
   return response.status(400).json({error: 'Project not found.'})
 }
 
@@ -44,7 +43,8 @@ const project = {
   title,
   url,
   techs,
-  likes
+  likes:repositories[projectIndex].likes,
+  
 };
 
 repositories[projectIndex] = project;
@@ -61,8 +61,9 @@ app.delete("/repositories/:id", (request, response) => {
   if(projectIndex <0){
     return response.status(400).json({error: 'Project not found.'});
 
-  }
+  }else{
     repositories.splice(projectIndex,1);
+  }
     
     return response.status(204).send();
 
@@ -74,21 +75,15 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const projectIndex = repositories.findIndex(project => project.id === id);
 
-  const project = repositories.find(project => project.id === id);
 
-  if(projectIndex <0){
+  if(projectIndex === -1){
     return response.status(400).json({error: 'Project not found.'});
   }
 
   
-  var aux = project.likes;
-  var x = Number(aux);
- 
-  project.likes = x+1;
-  
-  repositories[projectIndex] = project;
+  repositories[projectIndex].likes += 1;
 
-  return response.json(repositories);
+  return response.json(repositories[projectIndex]);
 
 });
 
